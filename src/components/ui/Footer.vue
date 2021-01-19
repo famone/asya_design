4d4b3ee5e21295ec938842cb4665803b04021e5b
Konstantin Timofeev <kt@webink.site>
2021-01-19 14:18:48 +0300

    commit

--- src/components/ui/Footer.vue
+++ src/components/ui/Footer.vue
@@ -2,40 +2,43 @@
 	<div>
 	<footer>
 		<div class="container">
-			<div class="col-lg-4 ma-20">
+			<div class="col-lg-6 ma-20">
 				<router-link tag="a" to="/">
 					<img src="../../assets/img/footer_logo.png" alt="" class="logo">
 				</router-link>
 	 			<p class="small-white">Asya Design — архитектурно-дизайнерское <br> бюро полного цикла. </p>
 			</div>
-			<div class="col-lg-4 ma-20">
+			<!-- <div class="col-lg-4 ma-20">
 				<ul>
 					<router-link tag="li" to="/uslugi">Виды работ </router-link>
 					<router-link tag="li" to="/projects">Проекты </router-link>
 					<router-link tag="li" to="/obo-mne">Обо Мне </router-link>
 					<router-link tag="li" to="/contacti">Контакты </router-link>
 				</ul>
-			</div>
-			<div class="col-lg-4 ma-20">
+			</div> -->
+			<div class="col-lg-3 ma-20">
 				<h5>Москва, <br>
 				Нижняя Сыромятническая улица, <br>
 				10с2</h5>
-				<h5><a href="">+7 905 506-05-37</a></h5>
 				<div class="socials2">
 					<a href=""><img src="../../assets/img/sc1.svg" alt=""></a>
 					<a href=""><img src="../../assets/img/sc2.svg" alt=""></a>
 					<a href=""><img src="../../assets/img/sc3.svg" alt=""></a>
 				</div>
 			</div>
+			<div class="col-lg-3 ma-20">
+				<p class="small-white">©2021 — Asya Design</p>
+				<p class="small-white webink"><a href="https://webink.site/">Разработка сайта — Webink.</a></p>
+			</div>
 		</div>
 	</footer>
-	<div class="copyright">
+	<!-- <div class="copyright">
 		<div class="container">
 			<div class="copy-box">
 				<p class="small-white">©2021 — Asya Design</p>
 				<p class="small-white webink"><a href="https://webink.site/">Разработка сайта — Webink.</a></p>
 			</div>
 		</div>
-	</div>
+	</div> -->
 	</div>
 </template>
